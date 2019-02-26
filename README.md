14738d0c7efd93d612ede15f9d2877fabf197fe1
Ted Mielczarek <ted@mielczarek.org>
2019-02-26 12:18:15 -0500

    add data, scripts, graphs

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 Firefox committers touching Rust code
 =====================================
 
+This repository contains data about the number of Rust vs. C/C++ commits
+and commit authors in Firefox and instructions for reproducing the output.
+
+
+Instructions
+============
+
 1. Clone the [`mozilla-central`](https://hg.mozilla.org/mozilla-central) Mercurial repository:
 
    `hg clone https://hg.mozilla.org/mozilla-central`
@@ -21,9 +28,26 @@ Firefox committers touching Rust code
    hg pushlogsync
    ```
 
-5. Run query:
+5. Run queries (in mozilla-central clone):
 
    ```
-   hg log -r 'filelog("glob:**.rs")' --template '{firstpushdate|isodatesec},{author|email},{node|short}\n' > firefox-rust-committers.csv
+   # List all commits that touched files ending in .rs.
+   TZ=UTC hg log -r 'filelog("glob:**.rs")' --template '{firstpushdate|hgdate},{author|email},{node|short}\n' > firefox-rust-committers.csv
+   # List all commits that touched files ending in C/C++ file extensions,
+   # starting from 2015-05-09, when the first Rust code landed in Firefox.
+   TZ=UTC hg log -r 'filelog("re:\.(c|cpp|cc|m|mm|h)") & firstpushdate(">2015-05-09")' --template '{firstpushdate|hgdate},{author|email},{node|short}\n' > firefox-cpp-committers.csv
    ```
 
+6. Create virtualenv:
+
+   ```
+   virtualenv venv
+   . venv/bin/activate
+   pip install arrow plotly
+   ```
+
+7. Run script to generate graphs:
+
+   ```
+   python committers.py firefox-rust-committers.csv firefox-cpp-committers.csv docs/index.html
+   ```
---
+++ committers.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python2
+
+from __future__ import print_function
+
+import arrow
+import csv
+import plotly
+import sys
+
+import plotly.graph_objs as go
+
+
+def parse_committers(path):
+    all_committers = set()
+    with open(path, 'rb') as f:
+        rows = [(arrow.get(int(row[0].split()[0])),
+                 row[1]) for row in csv.reader(f)]
+    for (start, end) in arrow.Arrow.span_range('month', arrow.get(2015, 5, 1),
+                                               arrow.utcnow()):
+        committers = set()
+        commits = 0
+
+        while rows and rows[0][0] < end:
+            (pushdate, user) = rows.pop(0)
+            commits += 1
+            committers.add(user)
+            all_committers.add(user)
+        yield (start, commits, len(committers), len(all_committers))
+
+
+def parse_all():
+    for ((month, cpp_commits, cpp_committers, total_cpp_committers),
+         (m2, rust_commits, rust_committers, total_rust_committers)) in zip(
+             parse_committers(sys.argv[2]), parse_committers(sys.argv[1])):
+        assert month == m2
+        # month.format('YYYY-MM')
+        yield (month, cpp_commits, cpp_committers, total_cpp_committers,
+               rust_commits, rust_committers, total_rust_committers)
+
+
+def write_charts():
+    data = list(parse_all())
+    print('%d months' % len(data))
+    dates = [d[0] for d in data]
+    fig = plotly.tools.make_subplots(rows=4, cols=1,
+                                     shared_xaxes=True,
+                                     print_grid=False,
+                                     subplot_titles=(
+                                         'Unique Commit Authors by Month',
+                                         'Total Unique Commit Authors',
+                                         'Commits by Language',
+                                     ))
+    fig['layout']['xaxis1'].update({
+        'type': 'date',
+    })
+    fig.append_trace(
+        go.Scatter(
+            x=dates,
+            y=[d[2] for d in data],
+            name='C/C++',
+        ), 1, 1)
+    fig.append_trace(
+        go.Scatter(
+            x=dates,
+            y=[d[5] for d in data],
+            name='Rust',
+        ), 1, 1)
+    fig.append_trace(
+        go.Scatter(
+            x=dates,
+            y=[d[3] for d in data],
+            yaxis='y2',
+            name='C/C++',
+        ), 2, 1)
+    fig.append_trace(
+        go.Scatter(
+            x=dates,
+            y=[d[6] for d in data],
+            yaxis='y2',
+            name='Rust',
+        ), 2, 1)
+    fig.append_trace(
+        go.Scatter(
+            x=dates,
+            y=[d[1] for d in data],
+            yaxis='y3',
+            name='C/C++',
+        ), 3, 1)
+    fig.append_trace(
+        go.Scatter(
+            x=dates,
+            y=[d[4] for d in data],
+            yaxis='y3',
+            name='Rust',
+        ), 3, 1)
+    fig.append_trace(
+        go.Scatter(
+            x=dates,
+            y=[d[1] for d in data],
+            yaxis='y4',
+            name='C/C++',
+            stackgroup='commits',
+            groupnorm='percent',
+        ), 4, 1)
+    fig.append_trace(
+        go.Scatter(
+            x=dates,
+            y=[d[4] for d in data],
+            yaxis='y4',
+            name='Rust',
+            stackgroup='commits',
+        ), 4, 1)
+    div = plotly.offline.plot(
+        fig,
+        output_type='div',
+        auto_open=False,
+        show_link=False,
+    )
+    with open(sys.argv[3], 'wb') as f:
+        f.write('''<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Firefox commit authors using Rust and C/C++</title>
+<style>
+html, body {{
+  width: 100%;
+  height: 100%;
+  margin: 0px;
+  padding: 0px;
+}}
+body > div {{
+  width: 100%;
+  height: 100%;
+}}
+</style>
+</head>
+<body>
+{}
+</button>
+</body>
+</html>
+'''.format(div))
+
+
+if __name__ == '__main__':
+    write_charts()
